5e3a2088208ee670ed814966a280189bc02b7651
张伟强 <zhangweiqiang@wxchina.com>
2018-08-09 19:52:37 +0800

    提交代码

--- gateway-system/gateway-zipkin/src/main/java/com/xuanwu/xtion/zipkin/ZipkinApplication.java
+++ gateway-system/gateway-zipkin/src/main/java/com/xuanwu/xtion/zipkin/ZipkinApplication.java
@@ -1,9 +1,11 @@
 package com.xuanwu.xtion.zipkin;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import zipkin.server.internal.EnableZipkinServer;
 
+@EnableAutoConfiguration
 @SpringBootApplication
 @EnableZipkinServer
 public class ZipkinApplication {
